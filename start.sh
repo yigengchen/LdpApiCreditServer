f09d8fba708a60d7d136fa1e261facab8d0ebfd0
chenyg <chenyg@asiainfo.com>
2015-12-02 17:50:53 +0800

    update

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#替换redis信息
+
+sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf
+sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf
+
+#替换mysql信息
+sed -i 's/MYSQL_SERVER_IP/'$MYSQL_PORT_3306_TCP_ADDR'/g'  ./config/userQuery.conf
+sed -i 's/MYSQL_SERVER_PORT/$MYSQL_PORT_3306_TCP_PORT/g'  ./config/userQuery.conf
+sed -i 's/MYSQL_SERVER_USERNAME/$MYSQL_USER/g'  ./config/userQuery.conf
+sed -i 's/MYSQL_SERVER_PASSWORD/$MYSQL_PASSWORD/g'  ./config/userQuery.conf
+sed -i 's/MYSQL_SERVER_DBNAME/$MYSQL_DATABASE/g'  ./config/userQuery.conf
+
+sed -i 's/LOCAL_PORT/8089/g'  ./config/userQuery.conf
+
+./LdpApiServer -c ./config/userQuery.conf -n query
