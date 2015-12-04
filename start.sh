98e6f4612889debb84fb66b0ba456a7d3d04cbe7
cherry4477 <cherry4477@users.noreply.github.com>
2015-12-04 10:58:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 #替换redis信息
 
-#sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf
+sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf
 sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf
 
 #替换mysql信息
@@ -11,7 +11,6 @@ sed -i 's/MYSQL_SERVER_PORT/'$MYSQL_PORT_3306_TCP_PORT'/g'  ./config/userQuery.c
 sed -i 's/MYSQL_SERVER_USERNAME/'$MYSQL_USER'/g'  ./config/userQuery.conf
 sed -i 's/MYSQL_SERVER_PASSWORD/'$MYSQL_PASSWORD'/g'  ./config/userQuery.conf
 sed -i 's/MYSQL_SERVER_DBNAME/'$MYSQL_DATABASE'/g'  ./config/userQuery.conf
-
 sed -i 's/LOCAL_PORT/8089/g'  ./config/userQuery.conf
-
+cd /usr/src/myapp && rm -Rf objs src
 ./LdpApiServer -c ./config/userQuery.conf -n query
